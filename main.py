f3eb5ecbc84b7ecd677e5b4ff6c05b978add0f73
francisco jesus perez medina <franperez159264@gmail.com>
2025-09-26 10:59:46 -0600

    Eliminar __pycache__ y otros archivos ignorados del seguimiento

--- main.py
+++
@@ -1,4 +0,0 @@
-from views.ventana_inicio import mostrar_ventana_inicio
-
-if __name__ == "__main__":
-    mostrar_ventana_inicio()
--- views/tema_bootstrap.py
+++
@@ -1,7 +0,0 @@
-import ttkbootstrap as tb
-
-def crear_ventana(titulo="Supermercado", tamaño="800x500", tema="flatly"):
-    root = tb.Window(themename=tema)
-    root.title(titulo)
-    root.geometry(tamaño)
-    return root
